0edcec546995ae55d35e9ec9289ce3474a249d26
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-11 13:00:40 +0300

    Homepage design

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -19,13 +19,13 @@ const Home = () => {
           <Widget type="earning" />
           <Widget type="balance" />
         </div>
+        <div className="listContainer">
+          <div className="listTitle">Projelerim</div>
+          <Table />
+        </div>
         <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2/1}/>
-        </div>
-        <div className="listContainer">
-          <div className="listTitle">Latest Transactions</div>
-          <Table />
         </div>
       </div>
     </div>
