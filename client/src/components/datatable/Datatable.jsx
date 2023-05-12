6ffefd718b8e07b60b1d5b267af6fd8864d63be9
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-12 10:20:03 +0300

    Projects page

--- client/src/components/datatable/Datatable.jsx
+++ client/src/components/datatable/Datatable.jsx
@@ -1,55 +1,84 @@
 import "./datatable.scss";
-import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-const Datatable = () => {
-  const [data, setData] = useState(userRows);
+const Datatable = ({ type }) => {
+  const [projects, setProjects] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const res = await axios.get(`/projects/findByUser/${currentUser._id}`);
+      setProjects(res.data);
+    };
+    fetchProjects();
+  }, [type]);
 
-  const actionColumn = [
-    {
-      field: "action",
-      headerName: "Action",
-      width: 200,
-      renderCell: (params) => {
-        return (
-          <div className="cellAction">
-            <Link to="/users/test" style={{ textDecoration: "none" }}>
-              <div className="viewButton">View</div>
-            </Link>
-            <div
-              className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
-            >
-              Delete
-            </div>
-          </div>
-        );
-      },
-    },
-  ];
   return (
-    <div className="datatable">
-      <div className="datatableTitle">
-        Add New User
-        <Link to="/users/new" className="link">
-          Add New
+    <TableContainer component={Paper} className="table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow className="tableHead">
+            <TableCell className="tableCell">Proje adı</TableCell>
+            <TableCell className="tableCell">Açıklama</TableCell>
+            <TableCell className="tableCell">Durum</TableCell>
+            <TableCell className="tableCell">Bakiye</TableCell>
+            <TableCell className="tableCell">Kar</TableCell>
+            <TableCell className="tableCell">Son ödeme</TableCell>
+            <TableCell className="tableCell">Son maliyet</TableCell>
+            <TableCell className="tableCell">Detay</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {projects.map((project) => (
+            <TableRow key={project._id} className="tableRow">
+              <TableCell className="tableCell">{project.title}</TableCell>
+              <TableCell className="tableCell">{project.desc}</TableCell>
+              <TableCell className=" cellWithStatus">
+                {project.status}
+              </TableCell>
+              <TableCell className="tableCell">{project.balance} ₺</TableCell>
+              <TableCell className="tableCell">{project.earning} ₺</TableCell>
+              <TableCell className="tableCell">
+                {project.costs.slice(-1).map((cost) => (
+                  <div key={cost.title}>
+                    <p>Başlık: {cost.title}</p>
+                    <p>Miktar: {cost.amount} ₺</p>
+                  </div>
+                ))}
+              </TableCell>
+              <TableCell className="tableCell">
+                {project.payments.slice(-1).map((payment) => (
+                  <div key={payment.title}>
+                    <p>Başlık: {payment.title}</p>
+                    <p>Miktar: {payment.amount} ₺</p>
+                  </div>
+                ))}
+              </TableCell>
+              <TableCell className="tableCell">
+                <Link to={`/projects/${project._id}`}>
+                  <button className="detailsButton">Detaylar</button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="addProjectButton">
+        <Link to={"/projects/new"}>
+          <button className="newProjectButton">Yeni Proje Ekle</button>
         </Link>
       </div>
-      <DataGrid
-        className="datagrid"
-        rows={data}
-        columns={userColumns.concat(actionColumn)}
-        pageSize={9}
-        rowsPerPageOptions={[9]}
-        checkboxSelection
-      />
-    </div>
+    </TableContainer>
   );
 };
 
