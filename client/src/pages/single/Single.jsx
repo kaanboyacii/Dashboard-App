f58fc81cb7730479f34f68a6921edf49f44aba04
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-13 12:40:37 +0300

    Single page changed

--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -2,11 +2,13 @@ import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
+import Datatable from "../../components/datatable/Datatable";
 import List from "../../components/table/Table";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { dislike, fetchSuccess, like } from "../../redux/projectSlice.js";
+import { fetchSuccess } from "../../redux/projectSlice.js";
+import Featured from "../../components/featured/Featured";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Single = () => {
@@ -67,29 +69,48 @@ const Single = () => {
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Bakiye:</span>
-                  <span className="itemValue">{absoluteBalance.toLocaleString()} ₺</span>
+                  <span className="itemValue">
+                    {absoluteBalance.toLocaleString()} ₺
+                  </span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Kar / Zarar:</span>
-                  <span className="itemValue">{earning.toLocaleString()} ₺</span>
+                  <span className="itemValue">
+                    {earning.toLocaleString()} ₺
+                  </span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Toplam Alınan Ödeme:</span>
-                  <span className="itemValue">{totalPayments.toLocaleString()} ₺</span>
+                  <span className="itemValue">
+                    {totalPayments.toLocaleString()} ₺
+                  </span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Toplam Maliyet:</span>
-                  <span className="itemValue">{totalCosts.toLocaleString()} ₺</span>
+                  <span className="itemValue">
+                    {totalCosts.toLocaleString()} ₺
+                  </span>
                 </div>
               </div>
             </div>
           </div>
           <div className="right">
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+            {/* <h1 className="title">Ödemeler ve Maliyetler</h1>
+            <Datatable project={currentProject} /> */}
           </div>
         </div>
         <div className="bottom">
-          <h1 className="title">Last Transactions</h1>
+          <div className="left">
+        
+            <Datatable/>
+          </div>
+          <div className="right">
+            <Featured />
+          </div>
+        </div>
+        <div className="end">
+          <h1 className="title">Son işlem gören projeler</h1>
           <List />
         </div>
       </div>
