a8cd6c09a38b6550859fbd2545c3fe504ece59d4
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-13 12:40:19 +0300

    Datatable changed

--- client/src/components/datatable/Datatable.jsx
+++ client/src/components/datatable/Datatable.jsx
@@ -1,84 +1,65 @@
 import "./datatable.scss";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
-const Datatable = ({ type }) => {
-  const [projects, setProjects] = useState([]);
+const Datatable = ({ project }) => {
   const { currentUser } = useSelector((state) => state.user);
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      const res = await axios.get(`/projects/findByUser/${currentUser._id}`);
-      setProjects(res.data);
-    };
-    fetchProjects();
-  }, [type]);
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "firstName", headerName: "First name", width: 130 },
+    { field: "lastName", headerName: "Last name", width: 130 },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
 
   return (
-    <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow className="tableHead">
-            <TableCell className="tableCell">Proje adı</TableCell>
-            <TableCell className="tableCell">Açıklama</TableCell>
-            <TableCell className="tableCell">Durum</TableCell>
-            <TableCell className="tableCell">Bakiye</TableCell>
-            <TableCell className="tableCell">Kar</TableCell>
-            <TableCell className="tableCell">Son ödeme</TableCell>
-            <TableCell className="tableCell">Son maliyet</TableCell>
-            <TableCell className="tableCell">Detay</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {projects.map((project) => (
-            <TableRow key={project._id} className="tableRow">
-              <TableCell className="tableCell">{project.title}</TableCell>
-              <TableCell className="tableCell">{project.desc}</TableCell>
-              <TableCell className=" cellWithStatus">
-                {project.status}
-              </TableCell>
-              <TableCell className="tableCell">{project.balance} ₺</TableCell>
-              <TableCell className="tableCell">{project.earning} ₺</TableCell>
-              <TableCell className="tableCell">
-                {project.costs.slice(-1).map((cost) => (
-                  <div key={cost.title}>
-                    <p>Başlık: {cost.title}</p>
-                    <p>Miktar: {cost.amount} ₺</p>
-                  </div>
-                ))}
-              </TableCell>
-              <TableCell className="tableCell">
-                {project.payments.slice(-1).map((payment) => (
-                  <div key={payment.title}>
-                    <p>Başlık: {payment.title}</p>
-                    <p>Miktar: {payment.amount} ₺</p>
-                  </div>
-                ))}
-              </TableCell>
-              <TableCell className="tableCell">
-                <Link to={`/projects/${project._id}`}>
-                  <button className="detailsButton">Detaylar</button>
-                </Link>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <div className="addProjectButton">
-        <Link to={"/projects/new"}>
-          <button className="newProjectButton">Yeni Proje Ekle</button>
-        </Link>
+    <div style={{ height: 400, width: "100%" }}>
+      <div className="datatableTitle">
+        <h2>Ödemeler ve Maliyetler</h2>
+        <div className="buttonContainer">
+          <button className="paymentButton">Yeni ödeme ekle</button>
+          <button className="costButton">Yeni maliyet ekle</button>
+        </div>
       </div>
-    </TableContainer>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
   );
 };
 
