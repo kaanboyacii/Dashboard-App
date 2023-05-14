8d06156027440237b11a136ee0aa35441c01631f
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-14 12:04:42 +0300

    Openable menu on single page

--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -3,12 +3,13 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import Datatable from "../../components/datatable/Datatable";
+import UpdateCosts from "../../components/update/UpdateCosts";
+import UpdatePayments from "../../components/update/UpdatePayments";
 import List from "../../components/table/Table";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { fetchSuccess } from "../../redux/projectSlice.js";
-import Featured from "../../components/featured/Featured";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const Single = () => {
@@ -16,6 +17,8 @@ const Single = () => {
   const dispatch = useDispatch();
   const path = useLocation().pathname.split("/")[2];
   const navigate = useNavigate();
+  const [openPayments, setOpenPayments] = useState(false);
+  const [openCosts, setOpenCosts] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,84 +47,98 @@ const Single = () => {
   let absoluteBalance = Math.abs(balance);
 
   return (
-    <div className="single">
-      <Sidebar />
-      <div className="singleContainer">
-        <Navbar />
-        <div className="top">
-          <div className="left">
-            <div className="editButton">Düzenle</div>
-            <h1 className="title">Bilgiler</h1>
-            <div className="item">
-              <div className="details">
-                <h1 className="itemTitle">{currentProject.title}</h1>
-                <div className="detailItem">
-                  <span className="itemKey">Açıklama:</span>
-                  <span className="itemValue">{currentProject.desc}</span>
+    <>
+      <div className="single">
+        <Sidebar />
+        <div className="singleContainer">
+          <Navbar />
+          <div className="top">
+            <div className="left">
+              <div className="editButton">Düzenle</div>
+              <h1 className="title">Bilgiler</h1>
+              <div className="item">
+                <div className="details">
+                  <h1 className="itemTitle">{currentProject.title}</h1>
+                  <div className="detailItem">
+                    <span className="itemKey">Açıklama:</span>
+                    <span className="itemValue">{currentProject.desc}</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Durum:</span>
+                    <span className="itemValue">{currentProject.status}</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">İletişim:</span>
+                    <span className="itemValue">{currentProject.contact}</span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Bakiye:</span>
+                    <span className="itemValue">
+                      {absoluteBalance.toLocaleString()} ₺
+                    </span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Kar / Zarar:</span>
+                    <span className="itemValue">
+                      {earning.toLocaleString()} ₺
+                    </span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Toplam Alınan Ödeme:</span>
+                    <span className="itemValue">
+                      {totalPayments.toLocaleString()} ₺
+                    </span>
+                  </div>
+                  <div className="detailItem">
+                    <span className="itemKey">Toplam Maliyet:</span>
+                    <span className="itemValue">
+                      {totalCosts.toLocaleString()} ₺
+                    </span>
+                  </div>
                 </div>
-                <div className="detailItem">
-                  <span className="itemKey">Durum:</span>
-                  <span className="itemValue">{currentProject.status}</span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">İletişim:</span>
-                  <span className="itemValue">{currentProject.contact}</span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Bakiye:</span>
-                  <span className="itemValue">
-                    {absoluteBalance.toLocaleString()} ₺
-                  </span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Kar / Zarar:</span>
-                  <span className="itemValue">
-                    {earning.toLocaleString()} ₺
-                  </span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Toplam Alınan Ödeme:</span>
-                  <span className="itemValue">
-                    {totalPayments.toLocaleString()} ₺
-                  </span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Toplam Maliyet:</span>
-                  <span className="itemValue">
-                    {totalCosts.toLocaleString()} ₺
-                  </span>
+              </div>
+              <div className="datatableTitle">
+                <div className="buttonContainer">
+                  <button
+                    onClick={() => setOpenPayments(true)}
+                    className="paymentButton"
+                  >
+                    Yeni ödeme ekle
+                  </button>
+                  <button
+                    onClick={() => setOpenCosts(true)}
+                    className="costButton"
+                  >
+                    Yeni maliyet ekle
+                  </button>
                 </div>
               </div>
             </div>
-            <div className="datatableTitle">
-              <div className="buttonContainer">
-                <button className="paymentButton">Yeni ödeme ekle</button>
-                <button className="costButton">Yeni maliyet ekle</button>
-              </div>
-            </div>
-          </div>
-          <div className="right">
-            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
-            {/* <h1 className="title">Ödemeler ve Maliyetler</h1>
+            <div className="right">
+              <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+              {/* <h1 className="title">Ödemeler ve Maliyetler</h1>
             <Datatable project={currentProject} /> */}
+            </div>
           </div>
-        </div>
-        <div className="bottom">
-          <div className="left">
-            <h1 className="title">Ödemeler</h1>
-            <Datatable type="payments" />
+          <div className="bottom">
+            <div className="left">
+              <h1 className="title">Ödemeler</h1>
+              <Datatable type="payments" />
+            </div>
+            <div className="right">
+              <h1 className="title">Maliyetler</h1>
+              <Datatable type="costs" />
+            </div>
           </div>
-          <div className="right">
-            <h1 className="title">Maliyetler</h1>
-            <Datatable type="costs" />
+          <div className="end">
+            <h1 className="title">Son işlem gören projeler</h1>
+            <List />
           </div>
-        </div>
-        <div className="end">
-          <h1 className="title">Son işlem gören projeler</h1>
-          <List />
         </div>
       </div>
-    </div>
+      {openPayments && <UpdatePayments setOpenPayments={setOpenPayments} />}
+      {openCosts && <UpdateCosts setOpenCosts={setOpenCosts} />}
+    </>
   );
 };
 
