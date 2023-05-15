852aecbb861e3455e97c2a2c8117a68601ce4ede
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-15 12:03:21 +0300

    Featured total show

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,44 +1,73 @@
-import "./featured.scss"
+import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Featured = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [totalEarning, setTotalEarning] = useState(0);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get(
+          `/projects/findByUser/${currentUser._id}`
+        );
+        const projects = response.data;
+
+        let total = 0;
+        projects.forEach((project) => {
+          total += project.earning;
+        });
+
+        setTotalEarning(total);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProjects();
+  }, []);
+
   return (
     <div className="featured">
       <div className="top">
-        <h1 className="title">Total Revenue</h1>
-        <MoreVertIcon fontSize="small"/>
+        <h1 className="title">Toplam Gelir</h1>
+        <MoreVertIcon fontSize="small" />
       </div>
       <div className="bottom">
         <div className="featuredChart">
-          <CircularProgressbar value={70} text={"70%"} strokeWidth={5}/>
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
         </div>
-        <p className="title">Total sales made today</p>
-        <p className="amount">$550</p>
-        <p className="desc">Previous transactions processing. Last payments may not be included</p>
+        <p className="title">Haftalık toplam gelir</p>
+        <p className="amount">{totalEarning.toLocaleString()} ₺</p>
+        <p className="desc">
+          Bu değer toplam kar zararı gösterir
+        </p>
         <div className="summary">
           <div className="item">
             <div className="itemTitle">Target</div>
             <div className="itemResult negative">
-              <KeyboardArrowDownIcon fontSize="small"/>
+              <KeyboardArrowDownIcon fontSize="small" />
               <div className="resultAmount">$12.5k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Week</div>
             <div className="itemResult positive">
-              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
               <div className="resultAmount">$2.7k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Month</div>
             <div className="itemResult positive">
-              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
               <div className="resultAmount">$15.1k</div>
             </div>
           </div>
@@ -48,4 +77,4 @@ const Featured = () => {
   );
 };
 
-export default Featured
+export default Featured;
