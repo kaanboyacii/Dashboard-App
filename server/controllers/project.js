efbaf977793ba115ed19268733d801b0d31df8c1
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-18 11:12:38 +0300

    Project delete payment & costs controller

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -14,52 +14,152 @@ export const addProject = async (req, res, next) => {
 
 export const updateProject = async (req, res, next) => {
     try {
-        const project = await Project.findById(req.params.id);
-        if (!project) return next(createError(404, "Project not found!"));
-        if (req.user.id === project.userId) {
-            const updatedProject = await Project.findByIdAndUpdate(
-                req.params.id,
-                {
-                    $set: req.body,
-                },
-                {
-                    new: true,
-                }
-            );
-
-            let totalCosts = 0;
-            updatedProject.costs.forEach((cost) => {
-                totalCosts += cost.amount;
-            });
-
-            let totalPayments = 0;
-            updatedProject.payments.forEach((payment) => {
-                totalPayments += payment.amount;
-            });
-
-            let earning = totalPayments - totalCosts;
-
-            let balance = earning >= 0 ? 0 : Math.abs(earning);
-
-            updatedProject.totalCosts = totalCosts;
-            updatedProject.totalPayments = totalPayments;
-
-            updatedProject.balance = balance;
-            updatedProject.earning = earning;
-
-            await updatedProject.save();
-
-            res.status(200).json(updatedProject);
-        } else {
-            return next(createError(403, "You can update only your Project!"));
-        }
+      const project = await Project.findById(req.params.id);
+      if (!project) return next(createError(404, "Project not found!"));
+      if (req.user.id === project.userId) {
+        const updatedProject = await Project.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          {
+            new: true,
+          }
+        );
+  
+        let totalCosts = 0;
+        updatedProject.costs.forEach((cost) => {
+          totalCosts += cost.amount;
+        });
+  
+        let totalPayments = 0;
+        updatedProject.payments.forEach((payment) => {
+          totalPayments += payment.amount;
+        });
+  
+        let earning = totalPayments - totalCosts;
+  
+        let balance = earning >= 0 ? 0 : Math.abs(earning);
+  
+        updatedProject.totalCosts = totalCosts;
+        updatedProject.totalPayments = totalPayments;
+  
+        updatedProject.balance = balance;
+        updatedProject.earning = earning;
+  
+        await updatedProject.save();
+  
+        res.status(200).json(updatedProject);
+      } else {
+        return next(createError(403, "You can update only your Project!"));
+      }
     } catch (err) {
-        next(err);
+      next(err);
     }
-};
-
-
-
+  };
+  
+  export const deletePayment = async (req, res, next) => {
+    try {
+      const projectId = req.params.id;
+      const paymentId = req.params.paymentId;
+  
+      const project = await Project.findById(projectId);
+      if (!project) {
+        return next(createError(404, "Proje bulunamadı!"));
+      }
+  
+      const payment = project.payments.find(
+        (payment) => payment._id.toString() === paymentId
+      );
+      if (!payment) {
+        return next(createError(404, "Ödeme bulunamadı."));
+      }
+  
+      if (req.user.id !== project.userId.toString()) {
+        return next(
+          createError(
+            403,
+            "Yalnızca kendi projenizin ödemesini silebilirsiniz!"
+          )
+        );
+      }
+  
+      project.payments = project.payments.filter(
+        (payment) => payment._id.toString() !== paymentId
+      );
+      const updatedProject = await project.save();
+  
+      // Güncelleme işlemi
+      await updateProjectCalculations(updatedProject);
+  
+      res.status(200).json("Ödeme silindi.");
+    } catch (error) {
+      console.error(error);
+      return next(createError(500, "Sunucu hatası"));
+    }
+  };
+  
+  export const deleteCost = async (req, res, next) => {
+    try {
+      const projectId = req.params.id;
+      const costId = req.params.costId;
+  
+      const project = await Project.findById(projectId);
+      if (!project) {
+        return next(createError(404, "Proje bulunamadı!"));
+      }
+  
+      const cost = project.costs.find((cost) => cost._id.toString() === costId);
+      if (!cost) {
+        return next(createError(404, "Maliyet bulunamadı."));
+      }
+  
+      if (req.user.id !== project.userId.toString()) {
+        return next(
+          createError(403, "Yalnızca kendi projenizin maliyetini silebilirsiniz!")
+        );
+      }
+  
+      project.costs = project.costs.filter(
+        (cost) => cost._id.toString() !== costId
+      );
+      const updatedProject = await project.save();
+  
+      // Güncelleme işlemi
+      await updateProjectCalculations(updatedProject);
+  
+      res.status(200).json("Maliyet silindi.");
+    } catch (error) {
+      console.error(error);
+      return next(createError(500, "Sunucu hatası"));
+    }
+  };
+  
+  // Proje hesaplamalarını güncelleme işlemi için yardımcı fonksiyon
+  const updateProjectCalculations = async (project) => {
+    let totalCosts = 0;
+    project.costs.forEach((cost) => {
+      totalCosts += cost.amount;
+    });
+  
+    let totalPayments = 0;
+    project.payments.forEach((payment) => {
+      totalPayments += payment.amount;
+    });
+  
+    let earning = totalPayments - totalCosts;
+  
+    let balance = earning >= 0 ? 0 : Math.abs(earning);
+  
+    project.totalCosts = totalCosts;
+    project.totalPayments = totalPayments;
+  
+    project.balance = balance;
+    project.earning = earning;
+  
+    await project.save();
+  };
+  
 export const deleteProject = async (req, res, next) => {
     try {
         const Project = await Project.findById(req.params.id)
@@ -82,6 +182,7 @@ export const deleteProject = async (req, res, next) => {
 
     }
 };
+
 
 export const getAllProject = async (req, res, next) => {
     try {
