a3dc14f07bc8577a6b31d9b332b3ef8e5bb473ef
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-22 11:27:52 +0300

    Navbar updated & fullscreen func

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,10 @@
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
-import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import { DarkModeContext } from "../../context/darkModeContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { logout } from "../../redux/userSlice.js";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
@@ -16,12 +13,38 @@ const Navbar = () => {
   const { dispatch } = useContext(DarkModeContext);
   const dispatchL = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   const handleLogout = async (e) => {
     window.location.href = "/";
     e.preventDefault();
     dispatchL(logout());
     const res = await axios.post("/auth/logout");
+  };
+
+  const handleFullscreen = () => {
+    if (!isFullscreen) {
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen();
+      } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+    setIsFullscreen(!isFullscreen);
   };
 
   return (
@@ -39,27 +62,21 @@ const Navbar = () => {
             />
           </div>
           <div className="item">
-            <FullscreenExitOutlinedIcon className="icon" />
+            <FullscreenExitOutlinedIcon
+              className="icon"
+              onClick={handleFullscreen}
+            />
           </div>
           <div className="item">
             <NotificationsNoneOutlinedIcon className="icon" />
             <div className="counter">1</div>
           </div>
           <div className="item">
-            <ChatBubbleOutlineOutlinedIcon className="icon" />
-            <div className="counter">2</div>
+            <img src={currentUser.img} alt="" className="avatar" />
           </div>
-          <div className="item">
-            <ListOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
-            <img
-              src={currentUser.img}
-              alt=""
-              className="avatar"
-            />
-          </div>
-          <button className="logout-button" onClick={handleLogout}>Çıkış yap</button>
+          <button className="logout-button" onClick={handleLogout}>
+            Çıkış yap
+          </button>
         </div>
       </div>
     </div>
