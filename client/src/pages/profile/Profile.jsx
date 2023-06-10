57556f4ae8e42dde4fd3e6e9c011489519e10105
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-06-10 17:11:09 +0300

    Profile update

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -3,36 +3,46 @@ import "./profile.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUser } from "../../redux/userSlice.js";
 
 const Profile = () => {
-  const { currentUser } = useSelector((state) => state.user);
+  const currentUser = useSelector((state) => state.user.currentUser);
   const [isEditing, setIsEditing] = useState(false);
-  const [name, setName] = useState(currentUser.name);
-  const [email, setEmail] = useState(currentUser.email);
-  const [password, setPassword] = useState(currentUser.password);
-  const [instagramUsername, setInstagramUsername] = useState(
-    currentUser.InstagramUsername
-  );
+  const [inputs, setInputs] = useState({ ...currentUser });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
 
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
-  const handleSaveClick = () => {
-    // Güncelleme işlemleri...
-    setIsEditing(false);
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
   };
 
+  const handleSaveClick = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(`/users/${currentUser._id}`, inputs);
+      setIsEditing(false);
+      if (res.status === 200) {
+        dispatch(updateUser(res.data)); // Güncellenen kullanıcı bilgilerini Redux store'a aktar
+        navigate(`/profile/${res.data._id}`, { replace: true });
+        setInputs({ ...currentUser });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const handleCancelClick = () => {
-    // Düzenlemeyi iptal etmek için değerleri eski haline getir
-    setName(currentUser.name);
-    setEmail(currentUser.email);
-    setPassword(currentUser.password);
-    setInstagramUsername(currentUser.InstagramUsername);
     setIsEditing(false);
   };
-
   return (
     <div className="profile">
       <Sidebar />
@@ -46,30 +56,34 @@ const Profile = () => {
                 <p>İsim:</p>
                 <input
                   type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
+                  value={inputs.name}
+                  onChange={handleChange}
                   className="inputField"
+                  name="name"
                 />
                 <p>E-posta:</p>
                 <input
                   type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  value={inputs.email}
+                  onChange={handleChange}
                   className="inputField"
+                  name="email"
                 />
                 <p>Şifre:</p>
                 <input
                   type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  value={inputs}
+                  onChange={handleChange}
                   className="inputField"
+                  name="password"
                 />
                 <p>Instagram URL:</p>
                 <input
                   type="text"
-                  value={instagramUsername}
-                  onChange={(e) => setInstagramUsername(e.target.value)}
+                  value={inputs.InstagramUsername}
+                  onChange={handleChange}
                   className="inputField"
+                  name="InstagramUsername"
                 />
                 <div className="buttonContainer">
                   <div className="saveButton" onClick={handleSaveClick}>
