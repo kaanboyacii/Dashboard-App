db8f76bcefc16f7f677a81ea7c8efb0c579af9a3
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-06-12 20:07:33 +0300

    costs  &payments style contaşner changed

--- client/src/pages/costs/Costs.jsx
+++ client/src/pages/costs/Costs.jsx
@@ -1,16 +1,17 @@
 import "./costs.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
-import Table from "../../components/table/Table"
 import { Link } from "react-router-dom"
 
 const List = () => {
   return (
     <div className="list">
-      <Sidebar/>
+      <Sidebar />
       <div className="listContainer">
-        <Navbar/>
-
+        <Navbar />
+        <div className="subListContainer">
+          <div className="listTitle">Maliyetler</div>
+        </div>
       </div>
     </div>
   )
--- client/src/pages/payments/Payments.jsx
+++ client/src/pages/payments/Payments.jsx
@@ -1,18 +1,17 @@
 import "./payments.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
-import Datatable from "../../components/datatable/Datatable"
-import Table from "../../components/table/Table"
 import { Link } from "react-router-dom"
 
 const List = () => {
   return (
     <div className="list">
-      <Sidebar/>
+      <Sidebar />
       <div className="listContainer">
-        <Navbar/>
-        
-   
+        <Navbar />
+        <div className="subListContainer">
+          <div className="listTitle">Ödemeler</div>
+        </div>
       </div>
     </div>
   )
