5a34a736098ac81648c08fc0d89b30ea1ce86e61
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-06-13 12:16:33 +0300

    Payments Page

--- client/src/pages/payments/Payments.jsx
+++ client/src/pages/payments/Payments.jsx
@@ -1,20 +1,63 @@
 import "./payments.scss"
-import Sidebar from "../../components/sidebar/Sidebar"
-import Navbar from "../../components/navbar/Navbar"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const List = () => {
+const Payments = () => {
+  const [payments, setPayments] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchPayments = async () => {
+      const res = await axios.get(`/projects/findByUser/${currentUser._id}`);
+      const projects = res.data;
+      const allPayments = projects.reduce((acc, project) => {
+        return [...acc, ...project.payments];
+      }, []);
+      setPayments(allPayments);
+    };
+    fetchPayments();
+  }, []);
+
   return (
     <div className="list">
       <Sidebar />
       <div className="listContainer">
         <Navbar />
         <div className="subListContainer">
-          <div className="listTitle">Ödemeler</div>
+          <div className="listTitle">Tüm Ödemeler</div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Başlık</TableCell>
+                  <TableCell>Miktar</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {payments.map((payment) => (
+                  <TableRow key={payment.title}>
+                    <TableCell>{payment.title}</TableCell>
+                    <TableCell>{payment.amount.toLocaleString()} ₺</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default Payments;
