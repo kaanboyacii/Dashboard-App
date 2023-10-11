bfdd89cdab7e7515eb8b30747152223f8754731f
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-11 11:38:40 +0300

    Statistic page data

--- client/src/pages/statistics/Statistics.jsx
+++ client/src/pages/statistics/Statistics.jsx
@@ -1,5 +1,5 @@
-// Statistics.js
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import axios from "axios";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
@@ -8,16 +8,26 @@ import "./statistics.scss";
 import Widget from "../../components/widget/Widget";
 
 const Statistics = () => {
-    const [projects, setProjects] = useState([]);
-    const { currentUser } = useSelector((state) => state.user);
-  
-    useEffect(() => {
-      const fetchProjects = async () => {
-        const res = await axios.get(`/projects/findByUser/${currentUser._id}`);
-        setProjects(res.data);
-      };
-      fetchProjects();
-    }, []);
+  const [projects, setProjects] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const res = await axios.get(`/projects/findByUser/${currentUser._id}`);
+      setProjects(res.data);
+    };
+    fetchProjects();
+  }, []);
+
+  // Calculate total earning and total balance
+  const totalEarning = projects.reduce(
+    (acc, project) => acc + project.earning,
+    0
+  );
+  const totalBalance = projects.reduce(
+    (acc, project) => acc + project.balance,
+    0
+  );
 
   return (
     <div className="list">
@@ -25,8 +35,28 @@ const Statistics = () => {
       <div className="listContainer">
         <Navbar />
         <div className="subListContainer">
-          <div className="listTitle">Tüm Ödemeler</div>
-         
+          <div className="listTitle">Tüm İstatistikler</div>
+          <div className="card">
+            <div className="card-title">Total Projects</div>
+            <div className="card-value">{projects.length}</div>
+            <Link to="/projects">
+              <div className="card-description">Detayları gör</div>
+            </Link>
+          </div>
+          <div className="card">
+            <div className="card-title">GENEL KAR</div>
+            <div className="card-value">{totalEarning.toLocaleString()}</div>
+            <Link to="/earning">
+              <div className="card-description">Detayları gör</div>
+            </Link>
+          </div>
+          <div className="card">
+            <div className="card-title">GENEL BAKİYE</div>
+            <div className="card-value">{totalBalance.toLocaleString()}</div>
+            <Link to="/balance">
+              <div className="card-description">Detayları gör</div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
