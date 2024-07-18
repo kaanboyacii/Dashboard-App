0f92183e3a335a7b2aaa243abb8bdbe97ff95a88
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2024-07-18 19:44:09 +0300

    Mini fixes in routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,13 +8,8 @@ import { verifyToken } from "../utility/verifyToken.js";
 
 const router = express.Router();
 
-//update user
 router.put("/:id", verifyToken, update);
-
-//delete user
 router.delete("/:id", verifyToken, deleteUser);
-
-//get a user
 router.get("/find/:id", getUser);
 
 export default router;
