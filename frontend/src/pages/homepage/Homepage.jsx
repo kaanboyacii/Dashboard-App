81832d4a1fc8bea3028899bfc080b6592bbf4d3f
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2024-07-25 10:30:43 +0300

    Homepage

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import Frontbase from "./../../layouts/frontbase/Frontbase";
+import "./homepage.scss";
 
 const Homepage = () => {
   return (
     <Frontbase>
       <div className="homepage-content">
-        <h2>Hoşgeldiniz!</h2>
-        <p>Bu ana sayfanın içeriği.</p>
+        <div className="widget-container">
+          <div className="widget">
+            <div className="widget-title">Toplam Proje:</div>
+            <div className="widget-value">15</div>
+            <button className="widget-button">İncele</button>
+          </div>
+          <div className="widget">
+            <div className="widget-title">Aylık Maliyet:</div>
+            <div className="widget-value">45500₺</div>
+            <button className="widget-button">İncele</button>
+          </div>
+          <div className="widget">
+            <div className="widget-title">Aylık Ödeme:</div>
+            <div className="widget-value">515000₺</div>
+            <button className="widget-button">İncele</button>
+          </div>
+          <div className="widget">
+            <div className="widget-title">Tahmini Kazanç:</div>
+            <div className="widget-value">15500₺</div>
+            <button className="widget-button">İncele</button>
+          </div>
+        </div>
+        <div className="chart-container">
+          <div className="chart-card">Chart 1</div>
+          <div className="chart-card">Chart 2</div>
+        </div>
       </div>
     </Frontbase>
   );
