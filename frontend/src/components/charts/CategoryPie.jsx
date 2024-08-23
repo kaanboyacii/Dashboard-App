35e4db290dbab2d7e1bd5f8ada601e596be459fb
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2024-08-23 14:42:58 +0300

    charts updated

--- frontend/src/components/charts/CategoryPie.jsx
+++ frontend/src/components/charts/CategoryPie.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { PieChart } from "@mui/x-charts/PieChart";
+import './categoryPie.scss'; 
 
 const CategoryPie = () => {
   const { currentProject } = useSelector((state) => state.project);
@@ -97,15 +98,31 @@ const CategoryPie = () => {
     "amount"
   );
 
+  const chartOptions = {
+    legend: {
+      display: false, // Legend'i gizle
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (tooltipItem) => tooltipItem.label + ': ' + tooltipItem.raw,
+        },
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    cutoutPercentage: 70,
+  };
+
   return (
-    <div style={{ display: "flex", justifyContent: "space-around" }}>
-      <div>
+    <div className="category-pie-container">
+      <div className="category-pie-chart">
         <h3>Maliyet Kategorileri</h3>
-        <PieChart series={[{ data: costData }]} width={400} height={400} />
+        <PieChart series={[{ data: costData }]} width={300} height={300} options={chartOptions} />
       </div>
-      <div>
+      <div className="category-pie-chart">
         <h3>Ödeme Kategorileri</h3>
-        <PieChart series={[{ data: paymentData }]} width={400} height={400} />
+        <PieChart series={[{ data: paymentData }]} width={300} height={300} options={chartOptions} />
       </div>
     </div>
   );
